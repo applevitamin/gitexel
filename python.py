fefc5c188bd71d34ceaebd0bc3f2b768d6b76cd8
apple.vitamin <profname@gmail.com>
2022-12-18 05:10:34 +0300

    списки

--- python.py
+++ python.py
@@ -1,4 +1,19 @@
-name=input("Как Вас зовут? ")
-age=int(input("Сколько Вам лет? "))
-print("Добрый день,", name+"!")
-print("Вы родились в,", 2022-age,"году!")
+#name=input("Как Вас зовут? ")
+#age=int(input("Сколько Вам лет? "))
+#print("Добрый день,", name+"!")
+#print("Вы родились в,", 2022-age,"году!")
+
+#список из чисел
+nums=[1,54,6,3,87,243,23,11]
+#Отображение содержимого списка 
+print("Список чисел",nums)
+#Длина чисел
+print("Длинна чисел", len(nums))
+#первый элемент
+print("first_element", nums[0])
+#Последний элемент
+print("last_element", nums[-1])
+#наибольшее значение
+print("max_nambers", max(nums))
+print("min_meaning", min(nums))
+print("summa", sum(nums))
